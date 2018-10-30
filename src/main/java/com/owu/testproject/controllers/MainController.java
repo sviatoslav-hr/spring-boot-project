22f97fa6db27a3c5530c92049985b4fe55a781a6
Sviatoslav <shot.ngh@gmail.com>
2018-10-30 16:39:19 +0200

    Initial commit

---
+++ src/main/java/com/owu/testproject/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.owu.testproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello");
+        return "index";
+    }
+}
+
